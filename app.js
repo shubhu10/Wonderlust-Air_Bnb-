e9af866b8cfcb545a0918de318b09a7541d8ed9e
shubham purohit <purohitshubham1144@gmail.com>
2024-06-23 15:53:39 +0530

    add database connection in app.js file

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express=require("express");
 const app= express();
 const mongoose=require("mongoose");
 
+
+
+let mongooseUrl="mongodb://127.0.0.1:27017/wanderlust"
+
+async function main(){
+    await mongoose.connect(mongooseUrl);
+}
+
+main().then((res)=>{
+    console.log("connect successfull");
+}).catch((err)=>{
+    console.log(err);
+});
+
 app.listen(1010,()=>{
     console.log("server is listening on port 1010");
 })
