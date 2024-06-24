da6799f0fac7204437510d69f8eb7c9b61b6c34e
shubham purohit <purohitshubham1144@gmail.com>
2024-06-24 16:07:19 +0530

    create navbar

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const Listing=require("./models/listing");
 const { assert } = require("console");
 const path=require("path");
 const methodOverride=require("method-override");
+const ejsMate=require("ejs-mate");
 
 
 app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname, "views"));
 app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate)
+app.use(express.static(path.join(__dirname,"/public")));
 
 let mongooseUrl="mongodb://127.0.0.1:27017/wanderlust"
 
